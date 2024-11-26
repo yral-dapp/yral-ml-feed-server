9ba2f8f4463d6f0194152aae619b3fd4534e5d4d
komal-rs <komalsai@gobazzinga.io>
2024-11-26 20:52:39 +0530

    offchain

--- rust_src/consts.rs
+++ rust_src/consts.rs
@@ -4,4 +4,8 @@ pub const ML_FEED_PY_SERVER: &str = "http://localhost:50059"; //"http://python_p
 #[cfg(not(feature = "local-bin"))]
 pub const ML_FEED_PY_SERVER: &str = "http://python_proc.process.yral-ml-feed-server.internal:50059";
 
-pub const OFF_CHAIN_AGENT: &str = "https://icp-off-chain-agent.fly.dev:443"; //"http://localhost:50051";
+#[cfg(feature = "local-bin")]
+pub const OFF_CHAIN_AGENT: &str = "https://pr-91-yral-dapp-off-chain-agent.fly.dev:443";
+
+#[cfg(not(feature = "local-bin"))]
+pub const OFF_CHAIN_AGENT: &str = "https://icp-off-chain-agent.fly.dev:443";
